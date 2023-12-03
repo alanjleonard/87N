8289e9cddfabbfc88c285c20370befda48edc61a
Al Leonard <aleonard@sparkgeo.com>
2023-12-03 15:45:40 -0500

    iNiTiAl CoMmIt

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+fetch("./js/menu.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Get the template from the script tag
+    const source = document.getElementById("template").innerHTML;
+
+    // Compile the template
+    const template = Handlebars.compile(source);
+
+    // Pass the data to the template
+    const html = template(data);
+
+    // Insert the generated HTML into the result div
+    document.getElementById("menu").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
+  });
